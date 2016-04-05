90e8fef360a3e923116742597f63abee63e529f7
yessika21289 <yessika21289@gmail.com>
2016-04-05 22:04:26 +0700

    Fixing issues.

--- application/views/admin/admin_login.php
+++ application/views/admin/admin_login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name=”robots” content=”noindex,nofollow”>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
@@ -29,7 +30,7 @@
     <![endif]-->
 </head>
 
-  <body class="admin-background" style="">
+  <body class="admin-background">
 
     <div class="container">
 
