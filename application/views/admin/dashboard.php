59f55cd28c3a9fd86d13f1fbb2ffa28f69d10468
yessika21289 <yessika21289@gmail.com>
2016-04-07 22:46:54 +0700

    Dashboard + bersih2.

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -4,98 +4,16 @@
         <!--overview start-->
         <div class="row">
             <div class="col-lg-12">
-                <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
+                <h3 class="page-header"><i class="fa fa-laptop"></i> Home</h3>
                 <ol class="breadcrumb">
-                    <li><i class="fa fa-home"></i><a href="<?php echo base_url(); ?>myadminkw">Home</a></li>
-                    <li><i class="fa fa-laptop"></i>Dashboard</li>
+                    <li><i class="fa fa-home"></i>Home</li>
                 </ol>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-3">
+        <h1>
+            Welcome, <?php echo $name; ?>!
+        </h1>
 
-                <div class="social-box facebook">
-                    <i class="fa fa-facebook"></i>
-                    <ul>
-                        <li>
-                            <strong>256k</strong>
-                            <span>friends</span>
-                        </li>
-                        <li>
-                            <strong>359</strong>
-                            <span>feeds</span>
-                        </li>
-                    </ul>
-                </div>
-                <!--/social-box-->
-            </div>
-            <div class="col-md-3">
-
-                <div class="social-box google-plus">
-                    <i class="fa fa-google-plus"></i>
-                    <ul>
-                        <li>
-                            <strong>962</strong>
-                            <span>followers</span>
-                        </li>
-                        <li>
-                            <strong>256</strong>
-                            <span>circles</span>
-                        </li>
-                    </ul>
-                </div>
-                <!--/social-box-->
-
-            </div>
-            <!--/col-->
-            <div class="col-md-3">
-
-                <div class="social-box twitter">
-                    <i class="fa fa-twitter"></i>
-                    <ul>
-                        <li>
-                            <strong>1562k</strong>
-                            <span>followers</span>
-                        </li>
-                        <li>
-                            <strong>2562</strong>
-                            <span>tweets</span>
-                        </li>
-                    </ul>
-                </div>
-                <!--/social-box-->
-
-            </div>
-            <!--/col-->
-
-        </div>
-
-
-        <div class="row">
-            <div class="col-md-6 portlets">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h2><strong>Calendar</strong></h2>
-
-                        <div class="panel-actions">
-                            <a href="#" class="wminimize"><i class="fa fa-chevron-up"></i></a>
-                            <a href="#" class="wclose"><i class="fa fa-times"></i></a>
-                        </div>
-
-                    </div>
-                    <br><br><br>
-
-                    <div class="panel-body">
-                        <!-- Widget content -->
-
-                        <!-- Below line produces calendar. I am using FullCalendar plugin. -->
-                        <div id="calendar"></div>
-
-                    </div>
-                </div>
-
-            </div>
-        </div>
 
     </section>
     <!--main content end-->
