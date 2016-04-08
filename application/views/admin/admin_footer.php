f61e09858de31fc7f508e6637c49632e54344807
yessika21289 <yessika21289@gmail.com>
2016-04-08 09:42:26 +0700

    Bersih2.

--- application/views/admin/admin_footer.php
+++ application/views/admin/admin_footer.php
@@ -8,49 +8,18 @@
 <!-- nice scroll -->
 <script src="<?php echo base_url(); ?>assets/js/jquery.scrollTo.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-<!-- charts scripts -->
-<script src="<?php echo base_url(); ?>assets/jquery-knob/js/jquery.knob.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery.sparkline.js" type="text/javascript"></script>
-<script src="<?php echo base_url(); ?>assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/owl.carousel.js"></script>
-<!-- jQuery full calendar -->
-<
-<script src="<?php echo base_url(); ?>assets/js/fullcalendar.min.js"></script> <!-- Full Google Calendar - Calendar -->
-<script src="<?php echo base_url(); ?>assets/fullcalendar/fullcalendar/fullcalendar.js"></script>
-<!--script for this page only-->
-<script src="<?php echo base_url(); ?>assets/js/calendar-custom.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery.rateit.min.js"></script>
-<!-- custom select -->
-<script src="<?php echo base_url(); ?>assets/js/jquery.customSelect.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/chart-master/Chart.js"></script>
 
 <!--custome script for all page-->
 <script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
-<!-- custom script for this page-->
-<script src="<?php echo base_url(); ?>assets/js/sparkline-chart.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/easy-pie-chart.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery-jvectormap-world-mill-en.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/xcharts.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery.autosize.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery.placeholder.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/gdp-data.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/morris.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/sparklines.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/charts.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/jquery.slimscroll.min.js"></script>
+
 <!--<script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>-->
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.uploadPreview.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/ckeditor/ckeditor.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/bootstrap-datepicker.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/bootstrap-datepicker.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/locales/bootstrap-datepicker.en-GB.min.js"></script>
 
 <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript" language="javascript"></script>-->
 <script src="<?php echo base_url(); ?>assets/multifile-master/jquery.MultiFile.js" type="text/javascript"
         language="javascript"></script>
 
-<script type="text/javascript" src="<?php echo base_url() . 'assets/tinymce/tinymce.min.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/tinymce/tinymce.min.js"></script>
 
 <script>
     tinymce.init({
@@ -117,50 +86,6 @@
         ]
     });
 
-    //knob
-    $(function () {
-        $(".knob").knob({
-            'draw': function () {
-                $(this.i).val(this.cv + '%')
-            }
-        })
-    });
-
-    //carousel
-    $(document).ready(function () {
-        $("#owl-slider").owlCarousel({
-            navigation: true,
-            slideSpeed: 300,
-            paginationSpeed: 400,
-            singleItem: true
-
-        });
-    });
-
-    //custom select box
-
-    $(function () {
-        $('select.styled').customSelect();
-    });
-
-    /* ---------- Map ---------- */
-    $(function () {
-        $('#map').vectorMap({
-            map: 'world_mill_en',
-            series: {
-                regions: [{
-                    values: gdpData,
-                    scale: ['#000', '#000'],
-                    normalizeFunction: 'polynomial'
-                }]
-            },
-            backgroundColor: '#eef3f7',
-            onLabelShow: function (e, el, code) {
-                el.html(el.html() + ' (GDP - ' + gdpData[code] + ')');
-            }
-        });
-    });
-
     // ====== upload song cover ======= //
     $(function () {
         $("#upload-cover-link").on('click', function (e) {
@@ -208,11 +133,6 @@
             label_field: "#upload-merchandise-link"
         });
     });
-
-    //    $('#sandbox-container .input-append.date').datepicker({
-    //        clearBtn: true,
-    //        todayHighlight: true
-    //    });
 
     function addAlbum() {
         $("#add-album-form").toggle('slow');
--- application/views/admin/admin_header.php
+++ application/views/admin/admin_header.php
@@ -19,24 +19,11 @@
     <!-- font icon -->
     <link href="<?php echo base_url(); ?>assets/css/elegant-icons-style.css" rel="stylesheet" />
     <link href="<?php echo base_url(); ?>assets/css/font-awesome.min.css" rel="stylesheet" />
-    <!-- full calendar css-->
-    <link href="<?php echo base_url(); ?>assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
-    <link href="<?php echo base_url(); ?>assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
-    <!-- easy pie chart-->
-    <link href="<?php echo base_url(); ?>assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>
-    <!-- owl carousel -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.css" type="text/css">
-    <link href="<?php echo base_url(); ?>assets/css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
-    <!-- Custom styles -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/fullcalendar.css">
 
-    <link href="<?php echo base_url(); ?>assets/css/widgets.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/css/style-responsive.css" rel="stylesheet" />
-    <link href="<?php echo base_url(); ?>assets/css/xcharts.min.css" rel=" stylesheet">
     <link href="<?php echo base_url(); ?>assets/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
-    <!-- Bootstrap Date Picker-->
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker3.css" rel="stylesheet">
+
     <link href="<?php echo base_url(); ?>assets/css/admin.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 -->
